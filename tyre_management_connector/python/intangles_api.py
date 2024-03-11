9cb18e85955453752653c2b6728279966a88acb0
NaviN <navin@aerele.in>
2024-03-11 17:46:03 +0530

    Get Fuel Alert Logs

--- tyre_management_connector/python/intangles_api.py
+++ tyre_management_connector/python/intangles_api.py
@@ -2,6 +2,7 @@ import frappe
 import json
 from datetime import datetime
 import requests
+from frappe import _
 
 @frappe.whitelist()
 def get_intangles_odometer_data(start_time=None,end_time=None,vehicle_no=None):
@@ -136,3 +137,56 @@ def get_vehicle_stoppage_log(start_time=None,end_time=None,vehicle_no=None,stopp
 		return reqd_data
 	else:
 		response.raise_for_status()
+
+@frappe.whitelist()
+def get_intangles_fuel_log(start_time=None,end_time=None):
+
+	if not start_time and not end_time:
+		time_obj = datetime.now()
+		start_time = time_obj.strftime("%Y-%m-%d 00:00:00")
+		end_time = time_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+	epoch = datetime(1970, 1, 1)
+	start_time_obj = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+	start_time_difference = start_time_obj - epoch
+	epoch_start_time=int(start_time_difference.total_seconds() * 1000)
+
+	end_time_obj = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+	end_time_difference = end_time_obj - epoch
+	epoch_end_time=int(end_time_difference.total_seconds() * 1000)
+	result=fuel_alert_log(
+		start_time=epoch_start_time,
+		end_time=epoch_end_time
+	)
+	while result.get('last_evaluated_timestamp'):
+		result=fuel_alert_log(
+			start_time=epoch_start_time,
+			end_time=epoch_end_time,
+			last_evaluated_timestamp=result.get('last_evaluated_timestamp')
+		)
+
+def fuel_alert_log(start_time=None, end_time=None, last_evaluated_timestamp=None):
+	connector_doc=frappe.get_single("Intangles Connector")
+	if last_evaluated_timestamp:
+		evaluated_timestamp = f"/{last_evaluated_timestamp}"
+	else:
+		evaluated_timestamp=""
+	url = f"{connector_doc.url}/api/v1/vendor/fuel_alert_logs/{connector_doc.account_id}/list/{start_time}/{end_time}{evaluated_timestamp}"
+	headers = {
+	  'vendor-access-token': connector_doc.get_password("vendor_access_token")
+	}
+	response = requests.request("GET", url, headers=headers)
+	if response.ok:
+		response=response.json().get('result')
+		if response.get('logs'):
+			post_fuel_logs(logs=response.get('logs'))
+		if response.get('paging',{}).get('isLastPage') == False:
+			result={
+				'last_evaluated_timestamp':response.get('paging',{}).get('lastEvaluatedTimestamp')
+			}
+			return result
+	else:
+		response.raise_for_status()
+
+def post_fuel_logs(logs):
+	frappe.log_error(message=logs, title=_("Fuel logs"))
